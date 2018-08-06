284c1be446ee0a6d5c0ef6813fe7ca8cca1a0901
Alex Harrison <AlexJHarrison@gmail.com>
2018-08-06 07:33:33 -0400

    initial files

---
+++ controllers/controller.js
@@ -0,0 +1,22 @@
+var express = require("express");
+
+var router = express.Router();
+
+var news = require("../models/news");
+var connections = require("../config/connection");
+
+
+//router.get
+
+
+
+
+module.exports = router;
+
+
+
+
+
+
+
+
